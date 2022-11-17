0cd66e91d4ab5bbd2c5778443ba860f6c98b4af3
adhmattheus <mattheusadhonnaysf@hotmail.com>
2022-11-17 11:21:36 -0300

    inserindo javascript

---
+++ main.js
@@ -0,0 +1,37 @@
+function createGame(player1, hour, player2) {
+  return `
+  <li>
+    <img src="./assets/icon-${player1}.png" alt="bandeira da ${player1}" />
+    <strong>${hour}</strong>
+    <img src="./assets/icon-${player2}.png" alt="bandeira de ${player2}" />
+  </li>  
+  `
+}
+
+function createCard(date, day, games) {
+  return `
+
+  <div class="card">
+      <h2>${date} <span>${day}</span></h2>
+
+      <ul>
+      ${games}            
+      </ul>
+    </div>
+  `
+}
+
+
+document.querySelector('#app').innerHTML = `
+
+  <header>
+    <img src="./assets/logo.svg.svg" alt="" />
+  </header >
+  <main id="cards">
+  
+    ${createCard("24/11", "quinta", createGame('brazil', '16:00', 'serbia'))}
+    ${createCard("28/11", "segunda", createGame('brazil', '13:00', 'switzerland'))}
+    ${createCard("02/12", "sexta", createGame('brazil', '16:00', 'cameroon'))}
+  
+  </main>
+  `
